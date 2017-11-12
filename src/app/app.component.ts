5eae4660bcfc6b0f89943e9bc00cfe44599b7d5b
jacobgives <jacobgives01@gmail.com>
2017-11-12 11:10:11 -0600

    first commit. Validations not working right.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { User } from './my-class';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  this_user;
+  user = new User();
+  users = [];
+  onSubmit(){
+    this.users.push(this.user);
+    this.user = new User();
+    this.this_user=this.users[this.users.length-1]
+  }
 }
---
+++ src/app/my-class.ts
@@ -0,0 +1,12 @@
+export class User {
+    first_name: string;
+    last_name: string;
+    email: string;
+    address: string;
+    unit_id: string;
+    password: string;
+    passconf:string;
+    city:string;
+    state:string;
+    luck:string;
+}
